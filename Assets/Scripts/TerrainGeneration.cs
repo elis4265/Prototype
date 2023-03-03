9f0ff24118062b23aef79dd7e83bbef030e8ae91
elis4265 <alicaondreakova@gmail.com>
2023-03-03 18:21:35 +0100

    Prototype with TestingTerrain scene and URP

---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+// TODO: Use coroutines instead of update : sign StartCoroutine()
+public class TerrainGeneration : MonoBehaviour
+{
+    public int width = 500; //x-axis of the terrain
+    public int height = 500; //z-axis
+
+    public int depth = 10; //y-axis
+
+    public float scale = 20f;
+
+    public float offsetX = 50f;
+    public float offsetY = 100f;
+
+    private void Start()
+    {
+        offsetX = Random.Range(0f, 9999f);
+        offsetY = Random.Range(0f, 9999f);
+    }
+
+    private void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain (TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight (int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
+
